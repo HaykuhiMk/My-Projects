8ada4ebc1a4d5cbf69419d182523d80e98564a2e
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-07-23 16:50:51 +0400

    Update client.h

--- Network-Programming/With-C++/Chat/Client/client.h
+++ Network-Programming/With-C++/Chat/Client/client.h
@@ -1,19 +1,20 @@
+#ifndef CLIENT_H
+#define CLIENT_H
 #include "conf.h"
 #include <string>
 #include <iostream>
-#ifndef CLIENT_H
-#define CLIENT_H
 
 class Client 
 {
-private:
-    SOCKET clientSocket;
-    struct addrinfo clientAddr;
-
 public:
     Client(const char*, const char*);
     ~Client();
     void sendMessage();
+
+private:
+    SOCKET clientSocket;
+    struct addrinfo clientAddr;
+
 };
 
 #endif      //CLIENT_H
