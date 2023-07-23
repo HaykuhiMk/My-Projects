014339844e300f5634be9c093d3f546d0a644eaf
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2023-07-23 16:50:09 +0400

    Update clnmain.cpp

--- Network-Programming/With-C++/Chat/Client/clnmain.cpp
+++ Network-Programming/With-C++/Chat/Client/clnmain.cpp
@@ -5,14 +5,11 @@
 int main() {
     std::string serverIP;
     std::string port;
-
     std::cout << "Enter the server IP address: ";
     std::cin >> serverIP;
     std::cout << "Enter the server port: ";
     std::cin >> port;
-
     Client client(serverIP.c_str(), port.c_str());
     client.sendMessage();
-
     return 0;
 }
