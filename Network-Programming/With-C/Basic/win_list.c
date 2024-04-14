54a5c62795961e4c64175adbce6c887c3852c638
Haykuhi Mkrtchyan <113100671+HaykuhiMk@users.noreply.github.com>
2024-04-14 17:36:27 +0400

    Update win_list.c

--- Network-Programming/With-C/Basic/win_list.c
+++ Network-Programming/With-C/Basic/win_list.c
@@ -10,38 +10,31 @@
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "iphlpapi.lib")
 
-int main() 
-{
+int main() {
     WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d)) 
-    {
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
         printf("Failed to initialize.\n");
         return -1;
     }
     DWORD asize = 20000;        //the size of adapters' address buffer.
     PIP_ADAPTER_ADDRESSES adapters;
-    do 
-    {
+    do {
         adapters = (PIP_ADAPTER_ADDRESSES)malloc(asize);
-        if (!adapters) 
-        {
+        if (!adapters) {
             printf("Couldn't allocate %ld bytes for adapters.\n", asize);
             WSACleanup();
             return -1;
         }
         int r = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, 0, adapters, &asize);
         printf("%d\n", r);
-        if (r == ERROR_BUFFER_OVERFLOW) 
-        {
+        if (r == ERROR_BUFFER_OVERFLOW) {
             printf("GetAdaptersAddresses wants %ld bytes.\n", asize);
             free(adapters);
         } 
-        else if (r == ERROR_SUCCESS) 
-        {
+        else if (r == ERROR_SUCCESS) {
             break;
         } 
-        else 
-        {
+        else {
             printf("Error from GetAdaptersAddresses: %d\n", r);
             free(adapters);
             WSACleanup();
@@ -50,12 +43,10 @@ int main()
     } 
     while (!adapters);
     PIP_ADAPTER_ADDRESSES adapter = adapters;
-    while (adapter) 
-    {
+    while (adapter) {
         printf("\nAdapter name: %S\n", adapter->FriendlyName);
         PIP_ADAPTER_UNICAST_ADDRESS address = adapter->FirstUnicastAddress;
-        while (address) 
-        {
+        while (address) {
             printf("\t%s", address->Address.lpSockaddr->sa_family == AF_INET ? "IPv4" : "IPv6");
             char ap[100];
             getnameinfo(address->Address.lpSockaddr, address->Address.iSockaddrLength, ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
